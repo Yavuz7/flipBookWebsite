dcb5f44ec8ccfd07fa7d92f3eefa887331e3167c
Yavuz7 <yavuzyurtseven1@gmail.com>
2023-12-22 16:06:51 -0500

    First Commit, still WIP

---
+++ app.js
@@ -0,0 +1,90 @@
+// UI
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+const book = document.querySelector("#book");
+// Pages
+const pages = document.querySelectorAll(".page");
+
+//Events
+prevBtn.addEventListener("click", goPrevPage);
+nextBtn.addEventListener("click", goNextPage);
+
+//Settings
+let pageIndex = 0;
+let transitionTimer = parseFloat(
+  getComputedStyle(book).getPropertyValue("--transition-timer")
+);
+//Timer For Page Turning, Change var in html
+transitionTimer = transitionTimer * 1000;
+let numOfPages = document.querySelectorAll(".page").length;
+
+// Initialize Z Values
+document.addEventListener("DOMContentLoaded", () => {
+  let i = 0;
+  for (const page of pages) {
+    page.style.zIndex = numOfPages - 1 - i;
+    i++;
+  }
+});
+
+function openBook() {
+  book.style.transform = "translateX(50%)";
+  prevBtn.style.transform = "translateX(-324px)";
+  nextBtn.style.transform = "translateX(324px)";
+}
+
+function closeBook(isAtBeginning) {
+  if (isAtBeginning) {
+    book.style.transform = "translateX(0%)";
+  } else {
+    book.style.transform = "translateX(100%)";
+  }
+  prevBtn.style.transform = "translateX(120px)";
+  nextBtn.style.transform = "translateX(-120px)";
+}
+
+function goNextPage() {
+  nextBtn.disabled = true;
+  prevBtn.disabled = true;
+
+  var currentPage = pages[pageIndex];
+  console.log();
+  if (pageIndex <= numOfPages - 1) {
+    currentPage.classList.add("flipped");
+    var zIndexChange = pageIndex - 1;
+    setTimeout(() => {
+      currentPage.style.zIndex = zIndexChange;
+      nextBtn.disabled = false;
+      prevBtn.disabled = false;
+    }, transitionTimer);
+
+    if (pageIndex == 0) {
+      openBook();
+    } else if (pageIndex == numOfPages - 1) {
+      closeBook(false);
+    }
+    pageIndex++;
+  }
+}
+
+function goPrevPage() {
+  prevBtn.disabled = true;
+  nextBtn.disabled = true;
+
+  var currentPage = pages[pageIndex - 1];
+  if (pageIndex >= 1) {
+    currentPage.classList.remove("flipped");
+    var zIndexChange = numOfPages - pageIndex;
+    currentPage.style.zIndex = zIndexChange;
+    setTimeout(() => {
+      prevBtn.disabled = false;
+      nextBtn.disabled = false;
+    }, transitionTimer);
+    if (pageIndex == 1) {
+      closeBook(true);
+    } else if (pageIndex == numOfPages) {
+      openBook();
+    }
+    pageIndex--;
+  }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+Code Derived From: https://github.com/codingstar-jason/3D-Book-Tutorial-Basic-CodingStar
+
+The original code had weird naming conventions and used switch statements so I decided to revamp it to make it easier to add pages. It's completely different code now!
